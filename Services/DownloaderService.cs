0f1e23290c68cf75ed2f21b2ca9d7dd4ba00c6f4
Mouseke <pomvanlieshout16@gmail.com>
2025-11-16 21:30:02 +0100

    Fixed DownloaderService it will handle UAC

--- Services/DownloaderService.cs
+++ Services/DownloaderService.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace MTGArtDownloader.Services
 {
@@ -24,15 +25,34 @@ namespace MTGArtDownloader.Services
                 UseShellExecute = true, // belangrijk voor GUI apps
             };
 
-            using var process = Process.Start(psi);
-            if (process == null)
+            try
             {
-                Console.WriteLine("Could not start the process.");
+                using var process = Process.Start(psi);
+                if (process == null)
+                {
+                    Console.WriteLine("Could not start the process.");
+                    return;
+                }
+
+                // Wacht async tot het proces klaar is
+                await process.WaitForExitAsync();
+            }
+            catch (Win32Exception ex) when (ex.NativeErrorCode == 1223)
+            {
+                // 1223 = The operation was canceled by the user (UAC/SmartScreen)
+                Console.WriteLine("The external application was blocked by Windows SmartScreen or cancelled.");
+                Console.WriteLine("To allow it to run:");
+                Console.WriteLine("1. Open File Explorer and navigate to the folder: " + AppConfig.ArtDownloaderPath);
+                Console.WriteLine("2. Right-click 'MTG Art Downloader.exe' and select 'Properties'.");
+                Console.WriteLine("3. At the bottom of the 'General' tab, check 'Unblock' if present, then click 'Apply' and 'OK'.");
+                Console.WriteLine("4. Run ScryForge again to start the downloader.");
                 return;
             }
-
-            // Wacht async tot het proces klaar is
-            await process.WaitForExitAsync();
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error starting external process: " + ex.Message);
+                return;
+            }
 
             // --- Post-processing: afbeeldingen verplaatsen ---
             var imageExtensions = new[] { ".png", ".jpg", ".webp" };
