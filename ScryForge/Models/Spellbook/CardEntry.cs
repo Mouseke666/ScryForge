b417796d462bce168f3cef504a57bfe9ce9215bc
Mouseke <pomvanlieshout16@gmail.com>
2026-03-03 06:20:33 +0100

    Fixed case sensitive issue

--- ScryForge/Models/Spellbook/CardEntry.cs
+++ ScryForge/Models/Spellbook/CardEntry.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace ScryForge.Models.Spellbook;
 
 public sealed class CardEntry
 {
+    [JsonPropertyName("card")]
     public string Card { get; init; } = string.Empty;
+
+    [JsonPropertyName("quantity")]
     public int Quantity { get; init; }
 }
--- ScryForge/Models/Spellbook/EstimateBracketRequest.cs
+++ ScryForge/Models/Spellbook/EstimateBracketRequest.cs
@@ -1,7 +1,13 @@
+
+using System.Text.Json.Serialization;
+
 namespace ScryForge.Models.Spellbook;
 
 public sealed class EstimateBracketRequest
 {
+    [JsonPropertyName("main")]
     public List<CardEntry> Main { get; init; } = new();
+
+    [JsonPropertyName("commanders")]
     public List<CardEntry> Commanders { get; init; } = new();
 }
